68e9fe40692ee30c094ad98359193a1872491422
Mammadov Javid <javidim@code.local>
2024-09-25 18:54:22 +0400

    first task

---
+++ script.js
@@ -0,0 +1,32 @@
+// let test = 'Ulfat'
+
+// switch (test) {
+//     case 'Sabir':
+//     case 'Mehman':
+//         console.log('1'); 
+        
+//         break;
+
+//     default:
+//         console.log('not found');
+// }
+
+
+
+
+
+// if (test === 'Sabir' || test === 'Mehman') {
+// console.log('1');    
+// } else { 
+//     console.log('not found');
+// }
+//let user = 'javid'
+ //console.log(user[0].toUpperCase()+ user.slice(1));
+//let user = 'javid'
+//console.log(user.slice(0,4)+user[4].toUpperCase() );
+//let user = 'javid'
+//console.log(user[0].toUpperCase()+user.slice(1))+user[2].toUpperCase()+user.slice(3,5);
+
+
+let user = 'javid'
+console.log(user.slice(0,2)+user[2].toUpperCase()+(user.slice(3,5)));
