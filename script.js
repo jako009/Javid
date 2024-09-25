729fb97ab43fd7211e96886f0cfdfa5742de6b9e
Mammadov Javid <javidim@code.local>
2024-09-25 20:15:53 +0400

    first task

--- script.js
+++ script.js
@@ -1,32 +1,68 @@
-// let test = 'Ulfat'
-
-// switch (test) {
-//     case 'Sabir':
-//     case 'Mehman':
-//         console.log('1'); 
-        
-//         break;
-
-//     default:
-//         console.log('not found');
-// }
-
-
-
-
-
-// if (test === 'Sabir' || test === 'Mehman') {
-// console.log('1');    
-// } else { 
-//     console.log('not found');
-// }
-//let user = 'javid'
- //console.log(user[0].toUpperCase()+ user.slice(1));
-//let user = 'javid'
-//console.log(user.slice(0,4)+user[4].toUpperCase() );
-//let user = 'javid'
-//console.log(user[0].toUpperCase()+user.slice(1))+user[2].toUpperCase()+user.slice(3,5);
+let a = "Javid"
+console.log(typeof(a))
+let b = 42
+console.log(typeof(b))
+let c = true
+console.log(typeof(c))
+let d
+console.log(typeof(d))
+let e = null
+console.log(typeof(e))
+let f = Symbol("mekteb")
+console.log(typeof(f))
+let g = BigInt(5^9999999)
+console.log(typeof(g));
 
 
-let user = 'javid'
-console.log(user.slice(0,2)+user[2].toUpperCase()+(user.slice(3,5)));
+
+let peach = 10 // let is the new way of declaring variables  
+
+const papa_johns = 50 // the const keyword was also introduced in the version to create constants/
+    // Once a constant is initialized, we cannot change its value 
+
+var watermelon = 20 // var is used in the older versions of javascript /
+
+
+let firstname = "Andrew"
+let Lastname = "Loko"
+let age = 44
+let country = "Czech"
+let hobby = "playing Guitar"
+let occupationa = "none"
+
+console.log(`My name is ${firstname} and surname is ${Lastname}, My age is ${age} i live in ${country} my hobby is ${hobby} and my occupation at the moment is ${occupationa}`)
+    // console.log({ Firstname: `Andrew`, Lastname: `Loko`, age: 44, country: `Czech`, hobby: `playing guitar`, occupation: `None` })
+
+
+
+
+
+
+
+let Address = 'Sumqayit'
+let num = 161
+
+console.log(`My address is ${Address} street, building number is ${num}.`)
+
+
+console.log(5 + 1, 5 - 3, 6 * 6, 18 / 2, 50 % 5)
+
+console.log(8===8 , 5 === '5') // true , false
+console.log( 7== '7', 7 == 5) // true , false 
+console.log(100 != 60) // true
+console.log(59 < 69, 55 < 44) //true , false
+console.log(6 !== 6, 4 !== '4') //false,true
+console.log(50 > 49, 15 > 10) // true ,false 
+console.log(10 >= 10, 8 >= 7) // true,true
+console.log(1<= 2, 69 <= 69) //false true
+
+
+
+console.log(true && true) //true
+console.log(true && false) //false
+
+console.log(true || false) //true
+
+console.log(!true) //false
+
+
